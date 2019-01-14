c9ea854fa9364364c787b01bdda4405d6cf75283
Eduardo Acacio <acaciosh@gmail.com>
2019-01-14 14:03:49 -0300

    new features

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -10,6 +10,8 @@ import { Restaurant } from "./restaurant/restaurant.model"
 import { MEAT_API } from '../app.api'
 import { ErrorHandler } from '../app.error-handler'
 
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
+
 @Injectable()
 export class RestaurantsService {
 
@@ -32,6 +34,13 @@ export class RestaurantsService {
   reviewsOfRestaurant(id: string): Observable<any>{
     return this.http
       .get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handlerError)
   }
