35e87b2a9ac386877ea6fc9dcff5f52215b2a81b
Eduardo Acacio <acaciosh@gmail.com>
2019-01-15 17:30:42 -0300

    adding order service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,10 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component'
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component'
+import { OrderService } from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +43,8 @@ import { RadioComponent } from './shared/radio/radio.component'
     OrderComponent,
     InputComponent,
     RadioComponent,
+    OrderItemsComponent,
+    DeliveryCostsComponent,
     
   ],
   imports: [
@@ -48,7 +53,7 @@ import { RadioComponent } from './shared/radio/radio.component'
     FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
